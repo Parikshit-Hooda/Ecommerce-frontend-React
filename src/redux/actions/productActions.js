d97e210b5ef42538372c2b2cdbdfdb0abc725db8
Parikshit Hooda <phooda804@live.com>
2022-02-02 17:58:07 +0530

    create action-types.js, productActions.js - Created actions in productActions.js . Dispatch called for setting a product, getting a selected product, removing a selected product - Created action-types.js .

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,5 +1,20 @@
-export const ActionTypes = {
-  SET_PRODUCTS: "SET_PRODUCTS",
-  SELECTED_PRODUCT: "SELECTED_PRODUCT",
-  REMOVE_SELECTED_PRODUCT: "REMOVE_SELECTED_PRODUCT"
+import { ActionTypes } from "../constants/action-types";
+
+export const setProducts = (products) => {
+  return {
+    type: ActionTypes.SET_PRODUCTS,
+    payload: products
+  };
+};
+
+export const selectedProduct = (product) => {
+  return {
+    type: ActionTypes.SELECTED_PRODUCT,
+    payload: product
+  };
+};
+export const removeSelectedProduct = () => {
+  return {
+    type: ActionTypes.REMOVE_SELECTED_PRODUCT
+  };
 };
