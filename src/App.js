167057ef3b6bbc58fddc12cd74a156f299e1a467
Parikshit Hooda <phooda804@live.com>
2022-02-02 17:50:45 +0530

    Initial commit - Setting up dependencies. - Include semantic-ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import "./styles.css";
+// import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Heelo</h1>
     </div>
   );
 }
